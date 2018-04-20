4ac9cd51410a556dd084f7ce4e6971bc15edc234
joonasmkauppinen <joonasm.kauppinen@gmail.com>
2018-04-20 12:37:31 +0300

    Changes to EV3Reciever class

--- Tuomo 2.0/src/pack/EV3Receive.java
+++ Tuomo 2.0/src/pack/EV3Receive.java
@@ -18,22 +18,22 @@ public class EV3Receive extends Thread {
 	private int controllerButtonInput;
 	
 	public EV3Receive() throws IOException {		
-		this.serv = new ServerSocket(1111);
-		this.s = serv.accept(); // Wait for laptop connection
-		this.in = new DataInputStream(s.getInputStream());
-		this.out = new DataOutputStream(s.getOutputStream());
 	}
 	
 	public void run() {
 		
-		connectionState("Connected!");
+		try {
+			this.serv = new ServerSocket(1111);
+			this.s = serv.accept(); // Wait for laptop connection
+			this.in = new DataInputStream(s.getInputStream());
+			this.out = new DataOutputStream(s.getOutputStream());
+			connectionState("Connected!");
+		} catch (IOException e) {}
 		
 		while(true) {
 			try {
 				controllerButtonInput = in.read();
-			} catch (IOException e1) {
-				
-			}
+			} catch (IOException e1) {}
 			
 			Delay.msDelay(10);
 		}
