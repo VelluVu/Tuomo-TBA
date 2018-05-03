6be4937174de012a9cda05db750aad591a3f3c88
joonasmkauppinen <joonasm.kauppinen@gmail.com>
2018-05-03 10:31:31 +0300

    Manuaali ohjaus yhdistetty mainiin

--- Tuomo 2.0/src/pack/EV3Receive.java
+++ Tuomo 2.0/src/pack/EV3Receive.java
@@ -1,54 +1,141 @@
 package pack;
 
 import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import lejos.hardware.Button;
 import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
+import lejos.hardware.motor.UnregulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.robotics.EncoderMotor;
 import lejos.utility.Delay;
 
-public class EV3Receive extends Thread {
-
-	private ServerSocket serv;
+public class EV3Receive {
+	
 	private Socket s;
+	private ServerSocket serv;
 	private DataInputStream in;
-	private DataOutputStream out;
-	private int controllerButtonInput;
 	
-	public EV3Receive() throws IOException {		
+	private EncoderMotor emLeft, emRight;
+	private EV3MediumRegulatedMotor clawMotor, liftMotor;
+	
+	public EV3Receive() {
+		
+		this.emLeft = new UnregulatedMotor(MotorPort.D);
+		this.emRight = new UnregulatedMotor(MotorPort.A);
+		
+		this.clawMotor = new EV3MediumRegulatedMotor(MotorPort.C);
+		this.liftMotor = new EV3MediumRegulatedMotor(MotorPort.B);
+		
 	}
+
 	
-	public void run() {
+	public void startManualControl() throws IOException {
 		
-		try {
-			this.serv = new ServerSocket(1111);
-			this.s = serv.accept(); // Wait for laptop connection
-			this.in = new DataInputStream(s.getInputStream());
-			this.out = new DataOutputStream(s.getOutputStream());
-			connectionState("Connected!");
-		} catch (IOException e) {}
+		try { this.serv = new ServerSocket(1111); } catch (IOException e) {}
+		LCD.drawString("Waiting PC...", 0, 2);
+		try { this.s = serv.accept(); } catch (IOException e) {}
 		
-		while(true) {
-			try {
-				controllerButtonInput = in.read();
-			} catch (IOException e1) {}
+		try { this.in = new DataInputStream(s.getInputStream()); } catch (IOException e) {}
+		
+		LCD.clear();
+		LCD.drawString("Waiting input", 2, 2);
+		
+		clawMotor.setSpeed(720);
+		liftMotor.setSpeed(80);
+		boolean isUp = false;
+		
+		while(!Button.ESCAPE.isDown()) {
 			
-			Delay.msDelay(10);
+			String controllerInput = in.readUTF();
+			String[] values = controllerInput.split(" ");
+			int leftMotor = Integer.parseInt(values[0]);
+			int rightMotor = Integer.parseInt(values[1]);
+			
+			drive(leftMotor, rightMotor);
+			
+			int controllerButtons = Integer.parseInt(values[2]);
+			int clawTacho = clawMotor.getTachoCount();
+			int liftTacho = liftMotor.getTachoCount();
+			
+			switch(controllerButtons) {
+			case 1:
+				if(clawTacho > -1780) {
+					clawMotor.backward();					
+				} else {
+					clawMotor.stop(true);
+				}
+				break;
+			case 2:
+				if(clawTacho < -85) {
+					clawMotor.forward();					
+				} else {
+					clawMotor.stop(true);
+				}
+				break;
+			case 3:
+				if(!isUp) {
+					if(liftTacho <= 73) {
+						liftMotor.forward();					
+					} else {
+						liftMotor.stop(true);
+					}					
+				}
+				
+				if(isUp) {
+					if(liftTacho >= 1) {
+						liftMotor.backward();
+					} else {
+						liftMotor.stop(true);
+					}
+				}
+				break;
+			default:
+				if(liftTacho >= 73 && !isUp) isUp = true;
+				if(liftTacho <= 1 && isUp) isUp = false;
+				
+				clawMotor.stop(true);
+				liftMotor.stop(true);
+			}
+			
+			
+			LCD.clear(4);
+			LCD.clear(5);
+			LCD.clear(6);
+			LCD.clear(7);
+			LCD.drawString("Left: " + leftMotor + "%", 0, 4);
+			LCD.drawString("Right: " + rightMotor + "%", 0, 5);
+			LCD.drawString("Button: " + controllerButtons, 0, 6);
+			LCD.drawString("CT:" + clawTacho + " LT:" + liftTacho, 0, 7);
+			LCD.refresh();
+			
+			Delay.msDelay(80);
 		}
-	}
-	
-	public int getContorllerButton() {
-		return controllerButtonInput;
-	}
-	
-	public void closeSockets() throws IOException {
+		
 		s.close();
 		serv.close();
+		liftMotor.close();
+		clawMotor.close();
 	}
 	
-	public void connectionState(String msg) {
-		LCD.drawString(msg, 0, 4);
+	public void drive(int leftM, int rightM) {
+		int leftPower = leftM;
+		int rightPower = rightM;
+		
+		emLeft.setPower(leftPower);
+		emRight.setPower(rightPower);
 	}
+
 }
+
+
+
+
+
+
+
+
+
