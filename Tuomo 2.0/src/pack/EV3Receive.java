c0d150676d8533a6af5f6639cac6461307d8d80a
joonasmkauppinen <joonasm.kauppinen@gmail.com>
2018-05-03 20:38:14 +0300

    Bluetooth latenssi fixattu?

--- Tuomo 2.0/src/pack/EV3Receive.java
+++ Tuomo 2.0/src/pack/EV3Receive.java
@@ -1,6 +1,7 @@
 package pack;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -13,93 +14,106 @@ import lejos.hardware.port.MotorPort;
 import lejos.utility.Delay;
 
 public class EV3Receive {
-	
+
 	private Socket s;
 	private ServerSocket serv;
 	private DataInputStream in;
-	
+	private DataOutputStream out;
+
 	private EV3LargeRegulatedMotor left, right;
 	private EV3MediumRegulatedMotor clawMotor, liftMotor;
-	
+
 	public EV3Receive() {
-		
+
 		this.left = new EV3LargeRegulatedMotor(MotorPort.D);
 		this.right = new EV3LargeRegulatedMotor(MotorPort.A);
-		
+
 		this.clawMotor = new EV3MediumRegulatedMotor(MotorPort.C);
 		this.liftMotor = new EV3MediumRegulatedMotor(MotorPort.B);
-		
+
 	}
 
-	
 	public void startManualControl() {
-		
+
 		// Tekee uuden socketin ja jää odottamaan että
 		// PC yhdistää IP osoitteeseen 10.0.1.1 ja porttiin 1111.
-		try { this.serv = new ServerSocket(1111); } catch (IOException e) {}
+		try {
+			this.serv = new ServerSocket(1111);
+		} catch (IOException e) {
+		}
 		LCD.drawString("Waiting PC...", 0, 2);
-		try { this.s = serv.accept(); } catch (IOException e) {}
-		
+		try {
+			this.s = serv.accept();
+		} catch (IOException e) {
+		}
+
 		// Avaa data input streamin
-		try { this.in = new DataInputStream(s.getInputStream()); } catch (IOException e) {}
-		
+		try {
+			this.in = new DataInputStream(s.getInputStream());
+			this.out = new DataOutputStream(s.getOutputStream());
+		} catch (IOException e) {
+		}
+
 		LCD.clear();
 		LCD.drawString("Waiting input", 2, 2);
-		
-		// Asettaa kouran moottorin tehon 100%:iin ja 
+
+		// Asettaa kouran moottorin tehon 100%:iin ja
 		// nosto moottorin tehon sopivaksi todettuun tehoon.
 		clawMotor.setSpeed(720);
 		liftMotor.setSpeed(80);
 		boolean isUp = false;
-		
-		while(!Button.ESCAPE.isDown()) {
-			
+
+		while (!Button.ESCAPE.isDown()) {
+
 			// Lukee datan jonka PC lähettää ja liittää sen muuttujaan.
 			String controllerInput = "";
-			try { controllerInput = in.readUTF(); } catch (IOException e) {}
-			
+			try {
+				controllerInput = in.readUTF();
+			} catch (IOException e) {}
+
 			// Data on muotoa "'teho' 'teho' 'ohjaimen nappi'", esim. "100 80 1".
 			// Split metodi leikkaa stringin kolmeen osaan tekee niistä Arrayn.
 			String[] values = controllerInput.split(" ");
-			
+
 			// Liitetään teho arvot omiin muuttujiin ja muutetaan ne stringistä integeriksi.
 			float leftMotor = Float.parseFloat(values[0]);
 			float rightMotor = Float.parseFloat(values[1]);
-			
+
 			// Moottorien ajo metodi.
 			drive(leftMotor, rightMotor);
-			
-			// Liitetään ohjaimen näppäinkomento omaan muuttujaansa ja muutetaan integeriksi.
+
+			// Liitetään ohjaimen näppäinkomento omaan muuttujaansa ja muutetaan
+			// integeriksi.
 			int controllerButtons = Integer.parseInt(values[2]);
 			int clawTacho = clawMotor.getTachoCount();
 			int liftTacho = liftMotor.getTachoCount();
-			
-			switch(controllerButtons) {
+
+			switch (controllerButtons) {
 			case 1:
-				if(clawTacho > -1780) {
-					clawMotor.backward();					
+				if (clawTacho > -1780) {
+					clawMotor.backward();
 				} else {
 					clawMotor.stop(true);
 				}
 				break;
 			case 2:
-				if(clawTacho < -85) {
-					clawMotor.forward();					
+				if (clawTacho < -85) {
+					clawMotor.forward();
 				} else {
 					clawMotor.stop(true);
 				}
 				break;
 			case 3:
-				if(!isUp) {
-					if(liftTacho <= 73) {
-						liftMotor.forward();					
+				if (!isUp) {
+					if (liftTacho <= 73) {
+						liftMotor.forward();
 					} else {
 						liftMotor.stop(true);
-					}					
+					}
 				}
-				
-				if(isUp) {
-					if(liftTacho >= 1) {
+
+				if (isUp) {
+					if (liftTacho >= 1) {
 						liftMotor.backward();
 					} else {
 						liftMotor.stop(true);
@@ -107,15 +121,15 @@ public class EV3Receive {
 				}
 				break;
 			default:
-				if(liftTacho >= 73 && !isUp) isUp = true;
-				if(liftTacho <= 1 && isUp) isUp = false;
-				
-				
+				if (liftTacho >= 73 && !isUp)
+					isUp = true;
+				if (liftTacho <= 1 && isUp)
+					isUp = false;
+
 				clawMotor.stop(true);
 				liftMotor.stop(true);
 			}
-			
-			
+
 			LCD.clear(4);
 			LCD.clear(5);
 			LCD.clear(6);
@@ -125,10 +139,15 @@ public class EV3Receive {
 			LCD.drawString("Button: " + controllerButtons, 0, 6);
 			LCD.drawString("CT:" + clawTacho + " LT:" + liftTacho, 0, 7);
 			LCD.refresh();
+
+			Delay.msDelay(50);
 			
-			Delay.msDelay(80);
+			try {
+				out.writeUTF(controllerInput);
+				out.flush();
+			} catch (IOException e) {}
 		}
-		
+
 		// Suljetaan bluetooth yhteys, socketti ja moottori portit.
 		try {
 			s.close();
@@ -139,36 +158,27 @@ public class EV3Receive {
 		liftMotor.close();
 		clawMotor.close();
 	}
-	
+
 	// Asettaa moottorien tehot ohjaimen arvojen mukaan.
 	public void drive(float leftM, float rightM) {
-		
+
 		float leftPower = (leftM / 100) * 720;
 		float rightPower = (rightM / 100) * 720;
-		
-		if(leftPower >= 0) {
+
+		if (leftPower >= 0) {
 			left.forward();
 		} else {
 			left.backward();
 		}
-		
-		if(rightPower >= 0) {
+
+		if (rightPower >= 0) {
 			right.forward();
 		} else {
 			right.backward();
 		}
-		
+
 		left.setSpeed(leftPower);
 		right.setSpeed(rightPower);
 	}
 
 }
-
-
-
-
-
-
-
-
-
